b7c15764b1ead02eed5146c7240e6bb760d708cd
j1098 <sungjun203@gmail.com>
2019-05-23 17:02:48 +0900

    comit by name

--- src/main/java/idu/cs/repository/UserRepository.java
+++ src/main/java/idu/cs/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package idu.cs.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import idu.cs.domain.User;
 
 public interface UserRepository 
 	extends JpaRepository<User, Long> {
-
+	List<User>findByName(String name);
+	List<User>findByNameOrderByIdAsc(String name);
+	List<User>findByCompany(String company);
 }
